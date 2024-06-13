4e5ea1d818f16d9eddd2650bdc2306ef96ae91ec
juane <juanantonioespinozasevilla@gmail.com>
2024-06-12 21:48:30 -0600

    primera clase

---
+++ parte_superior.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="Eglish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="proyecto III gen I" content="Pagina de prueba">
+   
+   
+   
+    <title>
+        Tercera I
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+
+
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <img src="./images/menu.png" alt="">
+        <p class="texto_menu_izquierda">
+            la casa de bob
+
+        </p>
+
+
+    </div>
+
+
+
+</menu>
